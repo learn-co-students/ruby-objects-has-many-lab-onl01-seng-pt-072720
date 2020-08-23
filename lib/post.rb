54660de5c0b6864e81c0652934c6534dca3e6fdb
danainjax <danainjax@gmail.com>
2020-08-23 18:48:09 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,29 @@
+class Post
+    @@all =[]
+
+    attr_accessor :title, :author
+
+    def initialize(title)
+        @title = title
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+    def author_name
+        if author
+            self.author.name
+        else
+            nil
+        end
+    end
+
+end
