0ea6cf4d056075e736a435dbebb5f07fbcb74ccb
Kelin Field <kfield111@hotmail.com>
2020-08-23 02:11:07 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Post
+
+      attr_accessor :author, :title, :genre
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else
+      self.author.name
+    end
+  end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Song
+
+      attr_accessor :artist, :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist == nil
+      return nil
+    else
+      self.artist.name
+    end
+  end
+
+
+end
