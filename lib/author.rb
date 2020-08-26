8d7a55730c4a666cf4678a6e479da17a889f9ab4
dcoppage86 <dakotakcoppage@me.com>
2020-08-26 13:28:25 -0600

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author
+    attr_accessor :name, :title
+
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        Post.all.select do |post|
+            post.author == self
+        end 
+    end
+
+    def add_post(title)
+        title.author = self
+    end
+
+    def add_post_by_title(title)
+        post = Post.new(title)
+        post.author == self
+    end
+
+    def self.post_count
+        Post.all.count 
+    end
+
+
+end
---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+    attr_accessor :author, :title
+
+    @@all = []
+
+    def initialize(name)
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
